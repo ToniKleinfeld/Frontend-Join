90aae659187ef3b8328223a9de90a4e5a685aa2a
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-03-20 14:02:56 +0100

    add most basic components for each section and add to router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: 'summary',
+        title: 'Summary',
+        loadComponent: () => import('./main/summary/summary.component').then(c => c.SummaryComponent),
+      },
+      {
+        path: 'addtask',
+        title: 'Add task',
+        loadComponent: () => import('./main/addtask/addtask.component').then(c => c.AddtaskComponent),
+      },
+      {
+        path: 'board',
+        title: 'Board',
+        loadComponent: () => import('./main/board/board.component').then(c => c.BoardComponent),
+      },
+      {
+        path: 'contacts ',
+        title: 'Contacts',
+        loadComponent: () => import('./main/contacts/contacts.component').then(c => c.ContactsComponent),
+      },
+    ],
+  },
+];
