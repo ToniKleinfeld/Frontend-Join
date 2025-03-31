fd54337a0ac800966d7e95647576ad93805bbdc0
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-03-31 12:52:42 +0200

    add responsive header start with sidenav

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,17 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
+  styleUrls: ['./header.component.scss','./header-mobile.component.scss']
 })
 export class HeaderComponent {
   constructor(private router: Router) {}
 
   dropDownMenu: boolean = false;
+  isMobile: boolean = window.innerWidth < 800;
 
   /**
    * Log out , return to Login side an delete all sessionStorage strings
@@ -22,4 +23,10 @@ export class HeaderComponent {
     sessionStorage.removeItem('authToken');
     this.router.navigate(['/login']);
   }
+
+  @HostListener('window:resize', [])
+onResize() {
+  this.isMobile = window.innerWidth < 800;
 }
+}
+
