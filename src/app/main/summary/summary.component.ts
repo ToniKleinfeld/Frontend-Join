e942dc6ebb68134dd64b1bac025f2dd3ecfba690
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-04-01 13:11:48 +0200

    add hover effects and how to generate the box , next step need backend api first to get info tasks , an so on

--- src/app/main/summary/summary.component.ts
+++ src/app/main/summary/summary.component.ts
@@ -1,71 +1,56 @@
-import { Component,HostListener } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-summary',
   imports: [],
   templateUrl: './summary.component.html',
-  styleUrl: './summary.component.scss'
+  styleUrl: './summary.component.scss',
 })
 export class SummaryComponent {
-
   isMobile: boolean = window.innerWidth < 1024;
 
+  /**
+   * Watch for window size change under 1024px, call funktion to swap array.object positions
+   */
   @HostListener('window:resize', [])
   onResize() {
-    this.isMobile = window.innerWidth < 1024;
+    const newIsMobile = window.innerWidth < 1024;
+    if (newIsMobile !== this.isMobile) {
+      this.isMobile = newIsMobile;
+      this.swapTasks(1, 2);
+    }
   }
 
-  summaryTasksDesktop = [
-    { task : "Tasks Urgent",
-      imgpath : "urgent.svg",
-      class: "largeBox"
+  summaryTasks = [
+    { task: 'Tasks Urgent', imgpath: 'urgent', class: 'largeBox' },
+    { task: 'Tasks in Board', imgpath: 'taskcount', class: 'smallBox' },
+    { task: 'Tasks To-do', imgpath: 'todo', class: 'smallBox toDo' },
+    {
+      task: 'Tasks in Progress',
+      imgpath: 'progresstasks',
+      class: 'smallBox',
     },
-    { task : "Tasks in Board",
-      imgpath : "taskcount.svg",
-      class: "smallBox"
-    },
-    { task : "Tasks To-do",
-      imgpath : "todo.svg",
-      class: "smallBox toDo"
-    },
-    { task : "Tasks in Progress",
-      imgpath : "progresstasks.svg",
-      class: "smallBox"
-    },
-    { task : "Awaiting Feedback",
-      imgpath : "feedback.svg",
-      class: "smallBox"
-    },
-    { task : "Tasks Done",
-      imgpath : "taskdone.svg",
-      class: "smallBox"
-    },
-  ]
+    { task: 'Awaiting Feedback', imgpath: 'feedback', class: 'smallBox' },
+    { task: 'Tasks Done', imgpath: 'taskdone', class: 'smallBox' },
+  ];
 
-  summaryTasksMobile = [
-    { task : "Tasks Urgent",
-      imgpath : "urgent.svg",
-      class: "largeBox"
-    },
-    { task : "Tasks To-do",
-      imgpath : "todo.svg",
-      class: "smallBox toDo"
-    },
-    { task : "Tasks in Board",
-      imgpath : "taskcount.svg",
-      class: "smallBox"
-    },
-    { task : "Tasks in Progress",
-      imgpath : "progresstasks.svg",
-      class: "smallBox"
-    },
-    { task : "Awaiting Feedback",
-      imgpath : "feedback.svg",
-      class: "smallBox"
-    },
-    { task : "Tasks Done",
-      imgpath : "taskdone.svg",
-      class: "smallBox"
-    },
-  ]
+  /**
+   * Swap the position of object in the Array for mobile version
+   *
+   * @param index1
+   * @param index2
+   */
+  swapTasks(index1: number, index2: number) {
+    if (
+      index1 >= 0 &&
+      index1 < this.summaryTasks.length &&
+      index2 >= 0 &&
+      index2 < this.summaryTasks.length
+    ) {
+      [this.summaryTasks[index1], this.summaryTasks[index2]] = [
+        this.summaryTasks[index2],
+        this.summaryTasks[index1],
+      ];
+    }
+  }
 }
