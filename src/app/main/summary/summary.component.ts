7fa170a848a291683d3177a5e3f62d4cc8d98e77
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-04-01 11:41:11 +0200

    add for loop to generate all tasks boxes when loading summary with two arrays mobile and desktop version

--- src/app/main/summary/summary.component.ts
+++ src/app/main/summary/summary.component.ts
@@ -14,4 +14,58 @@ export class SummaryComponent {
   onResize() {
     this.isMobile = window.innerWidth < 1024;
   }
+
+  summaryTasksDesktop = [
+    { task : "Tasks Urgent",
+      imgpath : "urgent.svg",
+      class: "largeBox"
+    },
+    { task : "Tasks in Board",
+      imgpath : "taskcount.svg",
+      class: "smallBox"
+    },
+    { task : "Tasks To-do",
+      imgpath : "todo.svg",
+      class: "smallBox toDo"
+    },
+    { task : "Tasks in Progress",
+      imgpath : "progresstasks.svg",
+      class: "smallBox"
+    },
+    { task : "Awaiting Feedback",
+      imgpath : "feedback.svg",
+      class: "smallBox"
+    },
+    { task : "Tasks Done",
+      imgpath : "taskdone.svg",
+      class: "smallBox"
+    },
+  ]
+
+  summaryTasksMobile = [
+    { task : "Tasks Urgent",
+      imgpath : "urgent.svg",
+      class: "largeBox"
+    },
+    { task : "Tasks To-do",
+      imgpath : "todo.svg",
+      class: "smallBox toDo"
+    },
+    { task : "Tasks in Board",
+      imgpath : "taskcount.svg",
+      class: "smallBox"
+    },
+    { task : "Tasks in Progress",
+      imgpath : "progresstasks.svg",
+      class: "smallBox"
+    },
+    { task : "Awaiting Feedback",
+      imgpath : "feedback.svg",
+      class: "smallBox"
+    },
+    { task : "Tasks Done",
+      imgpath : "taskdone.svg",
+      class: "smallBox"
+    },
+  ]
 }
