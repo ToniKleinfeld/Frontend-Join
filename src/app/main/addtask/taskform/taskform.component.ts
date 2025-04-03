4d5c109bd02a444b244e4794f35cf5ee91b327d5
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-04-03 16:13:25 +0200

    add prio button design and function

--- src/app/main/addtask/taskform/taskform.component.ts
+++ src/app/main/addtask/taskform/taskform.component.ts
@@ -1,12 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-taskform',
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './taskform.component.html',
   styleUrl: './taskform.component.scss'
 })
 export class TaskformComponent {
 showAssingedToList:boolean = false;
+
+addtaskdata = {
+  prio : 'medium',
+}
+
 }
