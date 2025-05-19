6e1902e56058a220de55e0f7209757a077df9dc4
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-05-19 14:13:24 +0200

    add styles to big card , set infos from task

--- src/app/main/board/fullcard/fullcard.component.ts
+++ src/app/main/board/fullcard/fullcard.component.ts
@@ -8,20 +8,21 @@ import {
 import { GetTaskData } from '../../../shared/interfaces/interfaces.model';
 import { InitialsPipe } from '../../../shared/pipes/initials.pipe';
 import { CommonModule } from '@angular/common';
+import { FormatUserNamePipe } from '../../../shared/pipes/format-user-name.pipe';
 
 @Component({
   selector: 'app-fullcard',
-  imports: [InitialsPipe, CommonModule],
+  imports: [InitialsPipe, CommonModule, FormatUserNamePipe],
   templateUrl: './fullcard.component.html',
   styleUrl: './fullcard.component.scss',
 })
 export class FullcardComponent {
   @Input() task!: GetTaskData;
-  @Output() clicked = new EventEmitter<void>();
+  @Output() closeOverlay = new EventEmitter<void>();
 
   editMode : boolean = false;
 
   onClick() {
-    this.clicked.emit();
+    this.closeOverlay.emit();
   }
 }
